aa3ccb4dfdc59ce473cb0938f97c9fae2eb2aa21
keven <kevenlass@web.de>
2021-03-23 09:05:46 +0100

    Insert and update query

--- src/Main/controller/Controller_Edit_Filtered_Customer.java
+++ src/Main/controller/Controller_Edit_Filtered_Customer.java
@@ -60,16 +60,29 @@ public class Controller_Edit_Filtered_Customer implements Initializable {
         }
     }
 
+    @FXML
     public void InsertAndUpdate() throws SQLException {
 
 
         new DatenbankHandler().Connect();
-
+        String Kundenname = Kunde_Textbox.getText().toString();
+        String ServiceBegin = ServiceAnfang_Textbox.getText().toString();
+        String ServiceEnde = ServiceEnde_Texbox.getText().toString();
+        String StandOrt = Standort_Textbox.getText().toString();
+        String Status = Status_Textbox.getText().toString();
+        String Seriennummer = Seriennummer_Textbox.getText().toString();
+        String Mac_Adresse = MacAdresse_Textbox.getText().toString();
+        String Twi_Nr = TwiNr_textbox.getText().toString();
+        String Liefertermin = Liefertermin_Textbox.getText().toString();
+        String ServiceLVL = ServiceLvl_Textbox.getText().toString();
+        String Lieferant = Lieferant_Textbox.getText().toString();
+        String Device_Name = Devicename_Textbox.getText().toString();
         String queryInstert = String.format(
                 "INSERT INTO History (Kunde,Standort,Device_Name,Status,Servicelvl,Lieferant,ServiceBegin,ServiceEnde,Liefertermin,Seriennummer,TWI_NR,MAC_Adresse) Values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')",
                 Kunde_Textbox.getText(), Standort_Textbox.getText(), Devicename_Textbox.getText(), Status_Textbox.getText(), ServiceLvl_Textbox.getText(), Lieferant_Textbox.getText(), ServiceAnfang_Textbox.getText(), ServiceEnde_Texbox.getText(), Liefertermin_Textbox.getText(), Seriennummer_Textbox.getText(), TwiNr_textbox.getText(), MacAdresse_Textbox.getText());
-        String queryUpdate = String.format("UPDATE DEVICE2(Kunde,Standort,Device_Name,Status,Servicelvl,Lieferant,ServiceBegin,ServiceEnde,Liefertermin,Seriennummer,TWI_NR,MAC_Adresse) Values ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s') where Mac_Adresse=" + MacAdresse_Textbox.getText() +")",
-        Kunde_Textbox.getText(), Standort_Textbox.getText(), Devicename_Textbox.getText(), Status_Textbox.getText(), ServiceLvl_Textbox.getText(), Lieferant_Textbox.getText(), ServiceAnfang_Textbox.getText(), ServiceEnde_Texbox.getText(), Liefertermin_Textbox.getText(), Seriennummer_Textbox.getText(), TwiNr_textbox.getText(), MacAdresse_Textbox.getText());
+        String queryUpdate = String.format(
+                "UPDATE DEVICE2 SET(Kunde,Standort,Device_Name,Status,Servicelvl,Lieferant,ServiceBegin,ServiceEnde,Liefertermin,Seriennummer,TWI_NR,MAC_Adresse) = (Select '%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s' from dual) where TWI_NR="+"'"+ Twi_Nr+"",
+                Kundenname,StandOrt, Device_Name, Status, ServiceLVL, Lieferant, ServiceBegin, ServiceEnde, Liefertermin, Seriennummer, Twi_Nr, Mac_Adresse);
 
         PreparedStatement stm = DatenbankHandler.connection.prepareStatement(queryInstert);
         PreparedStatement stm2 = DatenbankHandler.connection.prepareStatement(queryUpdate);
